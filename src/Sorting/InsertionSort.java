2bbc22fcb83950585cbbbff4aeb84767c4e4d497
Vibhurathore09 <vibhu.rathore_cs20@gla.ac.in>
2022-03-30 23:31:54 +0530

    init

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,5 +1,7 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class InsertionSort
 {
     void insertionSort(int arr[]) // Complexity O(n^2)
@@ -19,7 +21,7 @@ public class InsertionSort
     }
     void printAll(int arr[])
     {
-        for(int i=0;i<arr.length-1;i++)
+        for(int i=0;i<arr.length;i++)
         {
             System.out.print(arr[i] + " ");
         }
@@ -29,7 +31,15 @@ public class InsertionSort
     public static void main(String[] args)
     {
         InsertionSort obj = new InsertionSort();
-        int arr[] = {1,10,20,11,23,32};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int arr[] =new int[n];
+        System.out.println("Enter elements" );
+        for(int i=0 ; i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
         obj.insertionSort(arr);
         System.out.println("Printing Sorted Array");
         obj.printAll(arr);
